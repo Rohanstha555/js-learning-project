21cf9dc834821d58dd164c2e66254b85c4e03141
MinatoSensei <stharohan00@gmail.com>
2025-11-09 18:52:13 +0545

    img gallery project done

--- projects/bg-color-change.js
+++ projects/bg-color-change.js
@@ -1,49 +1,49 @@
 const button = document.querySelectorAll(".box");
 const body = document.body;
 
-// button.forEach( (btn) => {
-//     console.log(btn);
-//     btn.addEventListener("click" , (e) => {
-//         console.log(e)
-//         console.log(e.target)
-//         if(e.target.id === "red"){
-//             body.style.backgroundColor = e.target.id;
-//         }
-//         if(e.target.id === "green"){
-//             body.style.backgroundColor = e.target.id;
-//         }
-//         if(e.target.id === "blue"){
-//             body.style.backgroundColor = e.target.id;
-//         }
-//         if(e.target.id === "black"){
-//             body.style.backgroundColor = e.target.id;
-//         }
-//         if(e.target.id === "beige"){
-//             body.style.backgroundColor = e.target.id;
-//         }
-//     })
-// })
+button.forEach( (btn) => {
+    console.log(btn);
+    btn.addEventListener("click" , (e) => {
+        console.log(e)
+        console.log(e.target)
+        if(e.target.id === "red"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "green"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "blue"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "black"){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === "beige"){
+            body.style.backgroundColor = e.target.id;
+        }
+    })
+})
 
-button.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const buttonId = e.target.id;
-    console.log(buttonId);
-    switch (buttonId) {
-      case "red":
-        body.style.backgroundColor = e.target.id;
-        break;
-      case "green":
-        body.style.backgroundColor = e.target.id;
-        break;
-      case "blue":
-        body.style.backgroundColor = e.target.id;
-        break;
-      case "black":
-        body.style.backgroundColor = e.target.id;
-        break;
-      case "beige":
-        body.style.backgroundColor = e.target.id;
-        break;
-    }
-  });
-});
+// button.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const buttonId = e.target.id;
+//     console.log(buttonId);
+//     switch (buttonId) {
+//       case "red":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "green":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "blue":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "black":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//       case "beige":
+//         body.style.backgroundColor = e.target.id;
+//         break;
+//     }
+//   });
+// });
